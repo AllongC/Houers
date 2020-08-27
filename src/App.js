052ad30082aac5f8d58466f42c32c63ddd0f03f5
杨腾祺 <3150475761@qq.com>
2020-08-27 19:38:21 +0800

    引入路由和增加目录

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom'
+import Home from './pages/home'
 
 class App extends Component {
+  componentDidMount() {
+    console.log(this.props);
+  }
   render() {
-    return <div>App</div>
+    return <div>
+      <Router>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Redirect to='/home' />
+        </Switch>
+      </Router>
+    </div>
   }
 }
 
