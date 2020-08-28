d3c7e129b326e27f8a3ca4ded164aa24e657f95c
杨腾祺 <3150475761@qq.com>
2020-08-28 15:59:00 +0800

    轮播图

---
+++ src/component/swiper/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+
+// 引入ant design 组件库
+import { Carousel } from 'antd-mobile';
+// 引入axios
+import axios from "axios";
+
+
+class Swiper extends Component {
+    state = {
+        swiper: [],
+        imgHeight: 176,
+    }
+    componentDidMount() {
+        // 获取轮播图
+        axios.get('http://157.122.54.189:9060/home/swiper').then(res => {
+            console.log(res);
+            this.setState({
+                swiper: res.data.body
+            })
+        })
+    }
+    render() {
+        const { swiper } = this.state
+        return <div>
+            {swiper.length && <Carousel
+                autoplay
+                infinite
+                beforeChange={(from, to) => console.log()}
+                afterChange={index => console.log()}
+            >
+                {swiper.map(item => (
+                    <a
+                        key={item.id}
+                        style={{ display: 'block', width: '100%', height: this.state.imgHeight }}
+                    >
+                        <img
+                            src={`http://157.122.54.189:9060` + item.imgSrc}
+                            alt=""
+                            style={{ width: '100%', verticalAlign: 'top' }}
+                            onLoad={() => {
+                                window.dispatchEvent(new Event('resize'));
+                                this.setState({ imgHeight: 'auto' });
+                            }}
+                        />
+                    </a>
+                ))}
+            </Carousel>}
+        </div>;
+    }
+}
+
+export default Swiper
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 
+// 引入轮播组件
+import Swiper from "../component/swiper";
+
+
 class Index extends Component {
     render() {
-        return <div>首页</div>
+        return <div>
+            {/* 1.0 轮播图 start */}
+            <Swiper></Swiper>
+            {/* 1.0 轮播图 end */}
+        </div>
     }
 }
 
