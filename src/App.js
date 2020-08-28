fd20afb80689c06128b6fa55df920bf482d5501b
杨腾祺 <3150475761@qq.com>
2020-08-28 09:09:39 +0800

    重定向

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
 import Home from './pages/home'
 
 class App extends Component {
   render() {
     return <div>
       <Router>
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Redirect to='/home' />
-        </Switch>
+        <Route path="/home" component={Home} />
+        <Redirect path="/" to='/home/index' />
       </Router>
     </div>
   }
