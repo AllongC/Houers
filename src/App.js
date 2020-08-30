cd1fce17e643e75d7fc3a5dc18dd44e46266af4b
杨腾祺 <3150475761@qq.com>
2020-08-30 19:44:05 +0800

    重定向优化

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
+import CityList from './pages/cityList'
 import Home from './pages/home'
 
 class App extends Component {
   render() {
     return <div>
       <Router>
+        <Route exact path="/">
+          <Redirect to='/home/index' />
+        </Route>
         <Route path="/home" component={Home} />
-        <Redirect path="/" to='/home/index' />
+        <Route path="/citylist" component={CityList} />
       </Router>
     </div>
   }
---
+++ src/pages/cityList/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class CityList extends Component {
+    render() {
+        return <div>城市列表</div>
+    }
+}
+
+export default CityList;
