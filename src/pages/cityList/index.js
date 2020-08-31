6269c5282979c27b4a490e93623f7a48b28f0379
杨腾祺 <3150475761@qq.com>
2020-08-31 20:25:25 +0800

    数据格式处理

--- src/pages/cityList/index.js
+++ src/pages/cityList/index.js
@@ -6,10 +6,32 @@ import { connect } from 'react-redux';
 import { instance as axios } from '../../untils/request';
 
 class CityList extends Component {
+    state = {
+        list: [],
+        letter: []
+    }
     async componentDidMount() {
         const res = await Promise.all([this.getHotCity(), this.getAllCity()])
         const hotCity = res[0].data.body
         const allCity = res[1].data.body
+        let list = [{ name: '当前定位', list: [this.props.cityName] }]
+        list.push({ name: '热门城市', list: hotCity.map(item => item.label) })
+        const letterArr = ['#', '热']
+        allCity.sort((a, b) => a.short.localeCompare(b.short))
+        allCity.forEach(item => {
+            const alphabet = item.short[0].toUpperCase();
+            const index = letterArr.indexOf(alphabet)
+            if (index == -1) {
+                letterArr.push(alphabet)
+                list.push({ name: alphabet, list: [item.label] })
+            } else {
+                list[index].list.push(item.label)
+            }
+        })
+        this.setState({
+            list,
+            letter: letterArr
+        })
     }
     getHotCity = () => {
         return axios.get('/area/hot')
