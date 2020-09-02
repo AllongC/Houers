8cdf35efb9653fe94ba7f5bd06b7ae8e8d7e5293
杨腾祺 <3150475761@qq.com>
2020-09-02 19:41:35 +0800

    地图初始化

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 import { NavBar, Icon } from 'antd-mobile';
-
+import { connect } from "react-redux";
+import MapCss from "./index.module.scss";
 
 class Map extends Component {
+    componentDidMount() {
+        var map = new window.BMap.Map("container");
+        map.centerAndZoom(this.props.cityName);
+    }
     render() {
         return <div>
+            {/*1.0 头部 start */}
             <NavBar
                 mode="light"
                 icon={<Icon type="left" />}
                 onLeftClick={this.props.history.goBack}
             >地图找房</NavBar>
+            {/*1.0 头部 end */}
+
+            {/* 2.0 地图 start */}
+            <div id="container" className={MapCss.map}></div>
+            {/* 2.0 地图 end */}
         </div>
     }
 }
 
-export default Map
+const mapStateToProps = (state) => {
+    return {
+        cityName: state.cityReducer.cityName,
+    }
+}
+
+export default connect(mapStateToProps)(Map)
