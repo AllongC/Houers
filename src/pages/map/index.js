9116346b6f5ad01b7eee0b6f814c985ece7b0c7a
杨腾祺 <3150475761@qq.com>
2020-09-03 18:31:46 +0800

    添加控件

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -5,14 +5,20 @@ import MapCss from "./index.module.scss";
 
 import { instance as axios } from '../../untils/request'
 
+const BMap = window.BMap
+
 class Map extends Component {
     async componentDidMount() {
-        var map = new window.BMap.Map("container");
+        var map = new BMap.Map("container");
         map.centerAndZoom(this.props.cityName);
+        // 添加带有定位的导航控件
+        var navigationControl = new BMap.NavigationControl();
+        map.addControl(navigationControl);
         // 获取城市id
         const { value } = (await (axios.get('/area/info', { params: { name: this.props.cityName } }))).data.body
         // 获取当前城市的租房定位
         const res = (await axios.get('/area/map', { params: { id: value } })).data.body
+        console.log(res);
     }
     render() {
         return <div>
